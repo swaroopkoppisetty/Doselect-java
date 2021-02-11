a37f2c5cc0d1e5c53a65d4bf84c5853ffd35397a
swaroopkoppisetty <70090571+swaroopkoppisetty@users.noreply.github.com>
2021-02-11 09:03:23 +0530

    Add files via upload

--- Source.java
+++ Source.java
@@ -1,42 +1,85 @@
+package problem2;
+import java.io.*;
 import java.util.*;
-class ArrayListOps
-{
-	public ArrayList<Integer> makeArrayListInt(int n)
-	{
-			ArrayList<Integer> list=new ArrayList<> (Arrays.asList(new Integer[n]));
-				
-			Collections.fill(list, 0);
-			System.out.println(list);
-			return list;
-	}
-	public ArrayList<Integer> reverseList(ArrayList<Integer> list)
-	{
-		
-			Collections.reverse(list);
-			System.out.println(list);
-			return list;
-	}
-	public ArrayList<Integer> changeList(ArrayList<Integer> list,int m,int n)
-	{
-		
-		
-			list.set(list.indexOf(m), n);
-			System.out.println(list);
-			return list;
-	}
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+class Activity{
+  //Implement Activity class here..
+  String string1;
+  String string2;
+  String operator;
 
-	
-}
-public class Source{
-	public static void main(String[] args) 
-	{
-		ArrayListOps obj=new ArrayListOps();
-		ArrayList<Integer> list=new ArrayList<Integer>(Arrays.asList(10,25,33,28,10,12));
-		obj.makeArrayListInt(4);
-		obj.reverseList(list);
-		obj.changeList(list,28,20);
-	}
+  public Activity(String string1,String string2,String operator)
+  {
+    this.string1=string1;
+     this.string2=string2;
+      this.operator=operator;
+    
+  }
 }
 
 
 
+public class Source {
+  //Implement the two function given in description in here...
+  public String handleException(Activity a)
+
+  {
+    try
+    {
+          if(a.string1.equals(null) || a.string2.equals(null))
+          {
+              throw new NullPointerException();
+              
+          }
+          if(!a.operator.equals("+") && !a.operator.equals("-"))
+          {
+                throw new Exception();
+                   
+          }
+    }
+    catch(NullPointerException e)
+    {
+        return "Null values found";
+    }
+    catch(Exception e)
+    {
+    	System.out.println(a.operator);
+    	 return a.operator;
+    }
+    return "No Exception Found";
+  }
+  
+  public String doOperation(Activity a)
+  {
+    String result=null;
+    this.handleException(a);
+    
+    switch(a.operator)
+    
+    {
+      case "+":
+             result= a.string1.concat(a.string2);
+              break;
+      case "-":
+              result=a.string1.replace(a.string2,"");
+              break;
+      default:
+            break;
+    }
+    
+    
+    return result;
+  }
+	public static void main(String args[] ) throws Exception {
+    //Write your own main to check the program...
+		
+		Activity obj=new Activity("helloworld","hello","+");
+		
+		Source obj1=new Source();
+		obj1.handleException(obj);
+		obj1.doOperation(obj);
+   
+	}
+}
